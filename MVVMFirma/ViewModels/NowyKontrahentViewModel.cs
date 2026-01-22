62f099261c79e3e22f1e98783d82fea09a067858
StudioKASS <134683307+StudioKASS@users.noreply.github.com>
2026-01-22 12:07:30 +0100

    Pełna aktualizacja projektu – podmiana plików

--- MVVMFirma/ViewModels/NowyKontrahentViewModel.cs
+++ MVVMFirma/ViewModels/NowyKontrahentViewModel.cs
@@ -1,16 +1,309 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MVVMFirma.Helper;
+using MVVMFirma.Models;
+using MVVMFirma.ViewModels.Abstract;
 
 namespace MVVMFirma.ViewModels
 {
-    public class NowyKontrahentViewModel : WorkspaceViewModel
+    public class NowyKontrahentViewModel : JedenViewModel<Kontrahent>, IDataErrorInfo
     {
         public NowyKontrahentViewModel()
+            : base()
         {
-            base.DisplayName = "Kontrahent";
+            DisplayName = "Nowy kontrahent";
+            Item = new Kontrahent();
+
+            TypyKontrahentow = new ObservableCollection<TypKontrahenta>(
+                fakturyEntities.TypKontrahenta.ToList());
+
+            CzyAktywny = true;
+            Kraj = "Polska";
         }
+
+        public ObservableCollection<TypKontrahenta> TypyKontrahentow { get; }
+
+        private string _nip;
+        private string _nazwaPelna;
+        private string _nazwaSkrocona;
+        private string _telefon;
+        private string _email;
+        private bool _czyAktywny;
+        private TypKontrahenta _selectedTypKontrahenta;
+        private string _ulica;
+        private string _nrDomu;
+        private string _nrLokalu;
+        private string _kodPocztowy;
+        private string _miasto;
+        private string _kraj;
+
+        public string NIP
+        {
+            get => _nip;
+            set
+            {
+                if (_nip != value)
+                {
+                    _nip = value;
+                    OnPropertyChanged(() => NIP);
+                }
+            }
+        }
+
+        public string NazwaPelna
+        {
+            get => _nazwaPelna;
+            set
+            {
+                if (_nazwaPelna != value)
+                {
+                    _nazwaPelna = value;
+                    OnPropertyChanged(() => NazwaPelna);
+                }
+            }
+        }
+
+        public string NazwaSkrocona
+        {
+            get => _nazwaSkrocona;
+            set
+            {
+                if (_nazwaSkrocona != value)
+                {
+                    _nazwaSkrocona = value;
+                    OnPropertyChanged(() => NazwaSkrocona);
+                }
+            }
+        }
+
+        public string Telefon
+        {
+            get => _telefon;
+            set
+            {
+                if (_telefon != value)
+                {
+                    _telefon = value;
+                    OnPropertyChanged(() => Telefon);
+                }
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    OnPropertyChanged(() => Email);
+                }
+            }
+        }
+
+        public bool CzyAktywny
+        {
+            get => _czyAktywny;
+            set
+            {
+                if (_czyAktywny != value)
+                {
+                    _czyAktywny = value;
+                    OnPropertyChanged(() => CzyAktywny);
+                }
+            }
+        }
+
+        public TypKontrahenta SelectedTypKontrahenta
+        {
+            get => _selectedTypKontrahenta;
+            set
+            {
+                if (_selectedTypKontrahenta != value)
+                {
+                    _selectedTypKontrahenta = value;
+                    OnPropertyChanged(() => SelectedTypKontrahenta);
+                }
+            }
+        }
+
+        public string Ulica
+        {
+            get => _ulica;
+            set
+            {
+                if (_ulica != value)
+                {
+                    _ulica = value;
+                    OnPropertyChanged(() => Ulica);
+                }
+            }
+        }
+
+        public string NrDomu
+        {
+            get => _nrDomu;
+            set
+            {
+                if (_nrDomu != value)
+                {
+                    _nrDomu = value;
+                    OnPropertyChanged(() => NrDomu);
+                }
+            }
+        }
+
+        public string NrLokalu
+        {
+            get => _nrLokalu;
+            set
+            {
+                if (_nrLokalu != value)
+                {
+                    _nrLokalu = value;
+                    OnPropertyChanged(() => NrLokalu);
+                }
+            }
+        }
+
+        public string KodPocztowy
+        {
+            get => _kodPocztowy;
+            set
+            {
+                if (_kodPocztowy != value)
+                {
+                    _kodPocztowy = value;
+                    OnPropertyChanged(() => KodPocztowy);
+                }
+            }
+        }
+
+        public string Miasto
+        {
+            get => _miasto;
+            set
+            {
+                if (_miasto != value)
+                {
+                    _miasto = value;
+                    OnPropertyChanged(() => Miasto);
+                }
+            }
+        }
+
+        public string Kraj
+        {
+            get => _kraj;
+            set
+            {
+                if (_kraj != value)
+                {
+                    _kraj = value;
+                    OnPropertyChanged(() => Kraj);
+                }
+            }
+        }
+
+        public bool WasSaved { get; private set; }
+
+        public override void Save()
+        {
+            if (!IsValid())
+                return;
+
+            var nowyKontrahent = new Kontrahent
+            {
+                NIP = NIP?.Trim(),
+                NazwaPelna = NazwaPelna?.Trim(),
+                NazwaSkrocona = NazwaSkrocona?.Trim(),
+                Telefon = string.IsNullOrWhiteSpace(Telefon) ? null : Telefon.Trim(),
+                Email = string.IsNullOrWhiteSpace(Email) ? null : Email.Trim(),
+                CzyAktywny = CzyAktywny,
+                IdTypuKontrahenta = SelectedTypKontrahenta?.IdTypuKontrahenta
+            };
+
+            var adres = new AdresKontrahenta
+            {
+                Ulica = Ulica?.Trim(),
+                NrDomu = NrDomu?.Trim(),
+                NrLokalu = string.IsNullOrWhiteSpace(NrLokalu) ? null : NrLokalu.Trim(),
+                KodPocztowy = KodPocztowy?.Trim(),
+                Miasto = Miasto?.Trim(),
+                Kraj = Kraj?.Trim(),
+                CzyDomyslny = true,
+                Kontrahent = nowyKontrahent
+            };
+
+            nowyKontrahent.AdresKontrahenta.Add(adres);
+            fakturyEntities.Kontrahent.Add(nowyKontrahent);
+            fakturyEntities.SaveChanges();
+
+            WasSaved = true;
+            OnRequestClose();
+        }
+
+        public string Error => null;
+
+        public string this[string name]
+        {
+            get
+            {
+                return name switch
+                {
+                    nameof(NIP) => StringValidator.RequiredAndLength(NIP, "NIP", 10, 10),
+                    nameof(NazwaPelna) => StringValidator.RequiredAndLength(NazwaPelna, "Nazwa pełna", 2, 160),
+                    nameof(NazwaSkrocona) => StringValidator.RequiredAndLength(NazwaSkrocona, "Nazwa skrócona", 2, 80),
+                    nameof(SelectedTypKontrahenta) => Validator.Required(SelectedTypKontrahenta, "Typ kontrahenta"),
+                    nameof(Ulica) => StringValidator.RequiredAndLength(Ulica, "Ulica", 2, 120),
+                    nameof(NrDomu) => StringValidator.RequiredAndLength(NrDomu, "Nr domu", 1, 20),
+                    nameof(KodPocztowy) => StringValidator.RequiredAndLength(KodPocztowy, "Kod pocztowy", 3, 12),
+                    nameof(Miasto) => StringValidator.RequiredAndLength(Miasto, "Miasto", 2, 100),
+                    nameof(Kraj) => StringValidator.RequiredAndLength(Kraj, "Kraj", 2, 80),
+                    nameof(Email) => ValidateEmail(),
+                    _ => null
+                };
+            }
+        }
+
+        private string ValidateEmail()
+        {
+            if (string.IsNullOrWhiteSpace(Email))
+                return null;
+
+            if (!Email.Contains("@"))
+                return "Email ma niepoprawny format.";
+
+            return null;
+        }
+
+        private bool IsValid()
+        {
+            foreach (var property in ValidatedProperties)
+            {
+                if (!string.IsNullOrEmpty(this[property]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static IEnumerable<string> ValidatedProperties =>
+            new[]
+            {
+                nameof(NIP),
+                nameof(NazwaPelna),
+                nameof(NazwaSkrocona),
+                nameof(SelectedTypKontrahenta),
+                nameof(Ulica),
+                nameof(NrDomu),
+                nameof(KodPocztowy),
+                nameof(Miasto),
+                nameof(Kraj),
+                nameof(Email)
+            };
     }
 }
--- MVVMFirma/ViewModels/WszyscyKontrahenciViewModel.cs
+++ MVVMFirma/ViewModels/WszyscyKontrahenciViewModel.cs
@@ -1,21 +1,49 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using MVVMFirma.Helper;
 using MVVMFirma.Models;
 using MVVMFirma.ViewModels.Abstract;
+using MVVMFirma.Views;
 
 namespace MVVMFirma.ViewModels
 {
     public class WszyscyKontrahenciViewModel : WszystkieViewModel<Kontrahent>
     {
+        private string _deleteId;
+
         public WszyscyKontrahenciViewModel()
         {
             DisplayName = "Kontrahenci";
             SetSortOptions(new[] { "Id", "NIP", "Nazwa pełna", "Nazwa skrócona" });
+            AddCommand = new BaseCommand(OpenAddDialog);
+            RefreshCommand = new BaseCommand(load);
+            DeleteByIdCommand = new BaseCommand(DeleteById);
+        }
+
+        public ICommand AddCommand { get; }
+
+        public ICommand RefreshCommand { get; }
+
+        public ICommand DeleteByIdCommand { get; }
+
+        public string DeleteId
+        {
+            get => _deleteId;
+            set
+            {
+                _deleteId = value;
+                OnPropertyChanged(() => DeleteId);
+            }
         }
 
         protected override IEnumerable<Kontrahent> LoadData()
         {
-            return fakturyEntities.Kontrahent.ToList();
+            return fakturyEntities.Kontrahent
+                .Include(k => k.TypKontrahenta)
+                .ToList();
         }
 
         protected override bool MatchesFilter(Kontrahent item, string filterText)
@@ -37,6 +65,88 @@ namespace MVVMFirma.ViewModels
                 "Nazwa skrócona" => descending ? query.OrderByDescending(k => k.NazwaSkrocona) : query.OrderBy(k => k.NazwaSkrocona),
                 _ => descending ? query.OrderByDescending(k => k.IdKontrahenta) : query.OrderBy(k => k.IdKontrahenta)
             };
+        }
+
+        private void OpenAddDialog()
+        {
+            var viewModel = new NowyKontrahentViewModel();
+            var dialog = new NowyKontrahentDialog
+            {
+                DataContext = viewModel,
+                Owner = Application.Current?.MainWindow
+            };
+
+            if (dialog.ShowDialog() == true)
+                load();
+        }
+
+        private void DeleteById()
+        {
+            if (string.IsNullOrWhiteSpace(DeleteId))
+            {
+                ShowMessageBox("Podaj id kontrahenta do usunięcia.");
+                return;
+            }
+
+            if (!int.TryParse(DeleteId.Trim(), out var id))
+            {
+                ShowMessageBox("Id kontrahenta musi być liczbą.");
+                return;
+            }
+
+            var kontrahent = fakturyEntities.Kontrahent
+                .FirstOrDefault(k => k.IdKontrahenta == id);
+
+            if (kontrahent == null)
+            {
+                ShowMessageBox("Nie znaleziono kontrahenta o podanym id.");
+                return;
+            }
+
+            var adresy = fakturyEntities.AdresKontrahenta
+                .Where(a => a.IdKontrahenta == id)
+                .ToList();
+
+            var faktury = fakturyEntities.FakturaSprzedazy
+                .Where(f => f.IdKontrahenta == id)
+                .ToList();
+
+            if (faktury.Any())
+            {
+                var fakturyIds = faktury.Select(f => f.IdFakturySprzedazy).ToList();
+                var pozycjeFaktur = fakturyEntities.PozycjaFakturySprzedazy
+                    .Where(p => fakturyIds.Contains(p.IdFakturySprzedazy))
+                    .ToList();
+
+                if (pozycjeFaktur.Any())
+                    fakturyEntities.PozycjaFakturySprzedazy.RemoveRange(pozycjeFaktur);
+
+                fakturyEntities.FakturaSprzedazy.RemoveRange(faktury);
+            }
+
+            var zamowienia = fakturyEntities.ZamowienieSprzedazy
+                .Where(z => z.IdKontrahenta == id)
+                .ToList();
+
+            if (zamowienia.Any())
+            {
+                var zamowieniaIds = zamowienia.Select(z => z.IdZamowieniaSprzedazy).ToList();
+                var pozycjeZamowien = fakturyEntities.PozycjaZamowieniaSprzedazy
+                    .Where(p => zamowieniaIds.Contains(p.IdZamowieniaSprzedazy))
+                    .ToList();
+
+                if (pozycjeZamowien.Any())
+                    fakturyEntities.PozycjaZamowieniaSprzedazy.RemoveRange(pozycjeZamowien);
+
+                fakturyEntities.ZamowienieSprzedazy.RemoveRange(zamowienia);
+            }
+
+            if (adresy.Any())
+                fakturyEntities.AdresKontrahenta.RemoveRange(adresy);
+
+            fakturyEntities.Kontrahent.Remove(kontrahent);
+            fakturyEntities.SaveChanges();
+            load();
         }
     }
 }
